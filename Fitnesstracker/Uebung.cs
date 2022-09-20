058ed54cd5a1be99a2b51065c5cf9bc362552808
Revo1702 <patyr2000@gmail.com>
2022-09-20 14:55:10 +0200

    First Commit

---
+++ Fitnesstracker/Uebung.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fitnesstracker
+{
+    class Uebung
+    {
+        private string _uebungsName { get; set; }
+        private double _gewicht { get; set; }
+        private int _wiederholungen { get; set; }
+        public Uebung(string uebungsname, double gewicht, int wiederholungen)
+        {
+            _uebungsName = uebungsname;
+            _gewicht = gewicht;
+            _wiederholungen = wiederholungen;
+        }
+        public Uebung()
+        {
+
+        }
+
+        public Uebung(string uname)
+        {
+            _uebungsName = uname;
+        }
+
+        public void WriteToDatabase()
+        {
+
+        }
+    }
+}
