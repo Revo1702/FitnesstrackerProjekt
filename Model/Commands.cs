f637d6f9c688ee241c5a671360c283c2a9552146
Revo1702 <patyr2000@gmail.com>
2022-10-10 15:20:14 +0200

    Added Commands Class and Retro for Sprint 1

--- Model/Commands.cs
+++ Model/Commands.cs
@@ -1,13 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using FitnesstrackerMVVMSwitching.Views;
+using Prism.Commands;
+using System;
 
 namespace Model
 {
     class Commands
     {
+        public interface ICloseWindows
+        {
+            Action Close { get; set; }
+        }
+        public Action Close { get; set; }
+        private DelegateCommand _closeCommand;
+
+        public DelegateCommand CloseCommand => _closeCommand ?? (_closeCommand = new DelegateCommand(NeuesTrainingStarten));
+
+        void NeuesTrainingStarten()
+        {
+            MainView mv = new MainView();
+            mv.Show();
+            Close?.Invoke();
+        }
         private Action<object> _action;
         private bool _canExecute;
         public Commands(Action<object> action, bool canExecute)
@@ -26,5 +39,14 @@ namespace Model
         {
             _action(parameter);
         }
+        public Commands()
+        { 
+        
+        }
+
+
+
+
     }
 }
+
