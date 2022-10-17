e3ed64d58d9e6dd0bc29c4bf6c66c0d0ce5010e2
TRyborz <thomas.ryborz@daimler.com>
2022-10-17 11:17:07 +0200

    Hinzufügen von BerechnungView und BerechnungViewModel

---
+++ FitnesstrackerMVVMSwitching/Views/BerechnungView.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FitnesstrackerMVVMSwitching.Views
+{
+    /// <summary>
+    /// Interaktionslogik für BerechnungView.xaml
+    /// </summary>
+    public partial class BerechnungView : Window
+    {
+        public BerechnungView()
+        {
+            InitializeComponent();
+        }
+    }
+}
