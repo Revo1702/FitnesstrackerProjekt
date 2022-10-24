90cd8c875030387457495d9b67fbe5b915153cfa
Revo1702 <patyr2000@gmail.com>
2022-10-24 11:19:46 +0200

    Repaired the Project

---
+++ FitnesstrackerMVVMSwitching/ViewModels/AddExerciseViewModel.cs
@@ -0,0 +1,51 @@
+﻿using Model;
+using Prism.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Controls;
+using System.Windows.Data;
+using static DataAccess.DBAccess;
+
+namespace FitnesstrackerMVVMSwitching.ViewModels
+{
+    class AddExerciseViewModel
+    {
+        private DelegateCommand _exerciseCommand;
+        StackPanel StackPanel = new StackPanel();
+
+
+        public DelegateCommand ExerciseCommand => _exerciseCommand ?? (_exerciseCommand = new DelegateCommand(Exercise));
+
+        void Exercise()
+        {
+
+        }
+
+        public AddExerciseViewModel()
+        {
+            List<Uebungen> uebungen = new List<Uebungen>(ReadAllExercises());
+            int zaehler = 0;
+            foreach (var i in uebungen)
+            {
+                Binding binding = new Binding
+                {
+                    Path = new System.Windows.PropertyPath("ExerciseCommand")
+                };
+                string trimmedName = String.Concat(i.Name.Where(c => !Char.IsWhiteSpace(c)));
+                Button btn = new Button
+                {
+                    Name = trimmedName,
+                    Content = i.Name,
+                    Width = 200,
+                    Height = 100
+                };
+                btn.SetBinding(Button.CommandProperty, binding);
+                Grid.SetColumn(btn, 0);
+                Grid.SetRow(btn, zaehler);
+                StackPanel.Children.Add(btn);
+                zaehler++;
+            }
+        }
+    }
+}
---
+++ FitnesstrackerMVVMSwitching/ViewModels/BerechnungViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnesstrackerMVVMSwitching.ViewModels
+{
+    class BerechnungViewModel
+    {
+        public BerechnungViewModel()
+        {
+
+        }
+    }
+}
